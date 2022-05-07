4e520f507b1d3ea6ac0b4fa315610d557309fff1
Roman <romangorshkovreme@gmail.com>
2022-05-07 22:02:01 +0300

    initial

---
+++ TestTaskStoriesAPI/TestTaskStoriesAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTaskStoriesAPI
+//
+//  Created by Roman Gorshkov on 07.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .magenta
+    }
+
+
+}
+
